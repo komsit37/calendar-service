c46b42dfbd2f7decaf4cf1b0e10e4fa9651fd72e
kchalermsook <k.chalermsook@gmail.com>
2017-02-18 00:19:08 +0800

    Delete test function

--- calendar-server/src/main/scala/com/folio/calendar/controller/CalendarAdminHttpController.scala
+++ calendar-server/src/main/scala/com/folio/calendar/controller/CalendarAdminHttpController.scala
@@ -16,11 +16,6 @@ case class HolidayListView(
   holidays: Seq[Holiday]
 )
 
-@Mustache("holiday")
-case class HolidayDetailView(
-                            holiday: Holiday
-                          )
-
 class CalendarAdminHttpController @Inject()(
   holidayService: HolidayService
 ) extends Controller {
@@ -32,14 +27,6 @@ class CalendarAdminHttpController @Inject()(
       Holiday(Calendar.Jpx, LocalDate.of(2017, 2, 14), Some("valentine's day")),
       Holiday(Calendar.Jpx, LocalDate.of(2017, 2, 15))
     ))
-  }
-
-  get("/holiday") {request: Request =>
-    //    holidayService.getHolidays(Calendar.Jpx)
-    //return dummy data for now
-    HolidayDetailView(
-      Holiday(Calendar.Jpx, LocalDate.of(2017, 2, 14), Some("valentine's day"))
-    )
   }
 
   get("/:*") { request: Request =>
