8c911abda20d2f915a6d62f99265b9d24af77470
Komsit Prakobphol <pkomsit@folio-sec.com>
2017-02-20 15:08:48 +0900

    add thrift feature test

--- calendar-server/src/test/scala/com/folio/calendar/CalendarServerFeatureTest.scala
+++ calendar-server/src/test/scala/com/folio/calendar/CalendarServerFeatureTest.scala
@@ -1,5 +1,7 @@
 package com.folio.calendar
 
+import java.time.LocalDate
+
 import com.folio.calendar.idl.{Calendar, CalendarService}
 import com.folio.calendar.model.HolidayRepo
 import com.twitter.finagle.http.Status
@@ -22,12 +24,30 @@ class CalendarServerFeatureTest extends FeatureTest with BeforeAndAfterEach {
     repo.deleteAll.value
   }
 
+  val Fri = "2017-02-17"
+  val Sat = "2017-02-18"
+  val Sun = "2017-02-19"
+  val NextMon = "2017-02-20"
+  val NextTue = "2017-02-21"
+
   "thrift" should {
-    "insert and get holiday" in {
-      val d = idl.Holiday(Calendar.Jpx, "2017-02-14", None)
+    "getHolidays" in {
+      val d = idl.Holiday(Calendar.Jpx, Fri, Some("note"))
       client.insertHoliday(d).value
       val res = client.getHolidays(Calendar.Jpx).value
       res should contain(d)
+    }
+    "getNextBusinessDay" in {
+      client.insertHoliday(idl.Holiday(Calendar.Jpx, NextMon)).value
+      client.getNextBusinessDay(Calendar.Jpx, Sat).value shouldBe NextTue
+      client.getNextBusinessDay(Calendar.Jpx, Sun).value shouldBe NextTue
+      client.getNextBusinessDay(Calendar.Jpx, NextMon).value shouldBe NextTue
+    }
+    "getPreviousBusinessDay" in {
+      client.insertHoliday(idl.Holiday(Calendar.Jpx, NextMon)).value
+      client.getPreviousBusinessDay(Calendar.Jpx, NextMon).value shouldBe Fri
+      client.getPreviousBusinessDay(Calendar.Jpx, Sun).value shouldBe Fri
+      client.getPreviousBusinessDay(Calendar.Jpx, Sat).value shouldBe Fri
     }
   }
 
